b1d4c664732d07bf7784a34264bbad5fdb90f565
Albert Oh <albertopha@hotmail.com>
2017-11-22 08:21:17 +0900

    frontend

--- api/index.js
+++ api/index.js
@@ -9,22 +9,16 @@ const Activity = db.Activity;
 module.exports = router;
 
 router.get('/', (req, res, next) => {
-    const places = Place.findAll({})
     const hotels = Hotel.findAll({ include: [{ all: true }] })
     const restaurants = Restaurant.findAll({ include: [{ all: true }] })
     const activities = Activity.findAll({ include: [{ all: true }] })
 
     Promise.all([hotels, restaurants, activities])
     .then(arr => {
-
         res.json({
             "hotels": arr[0],
             "restaurants": arr[1],
             "activities": arr[2]
         })
-
-        // var result = [];
-        // arr.forEach(element => result.push(element));
-        // res.json(result);
     })
 });
